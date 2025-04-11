7ae9438d15836e3666986efb94deac43205081da
Katuhinx <145397681+Katuhinx@users.noreply.github.com>
2025-04-11 21:48:09 +0300

    Update adi_mpi.c

--- adi_mpi.c
+++ adi_mpi.c
@@ -4,7 +4,6 @@
 #include <sys/time.h>
 #include <mpi.h>
 
-
 #include "adi.h"
 
 double second();
@@ -13,8 +12,8 @@ DATA_TYPE **X;
 DATA_TYPE **A;
 DATA_TYPE **B;
 int local_rows;
-int num_quanta = 1;// количество квантов 
-int quantum_size;// количество элементов на 1 кванте
+int num_quanta = 1; // количество квантов
+int quantum_size;   // количество элементов на 1 кванте
 
 // Функция инициализации массивов X, A и B
 static void init_arrays()
@@ -24,11 +23,11 @@ static void init_arrays()
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    X = (DATA_TYPE **)malloc(local_rows * sizeof(DATA_TYPE *));
-    A = (DATA_TYPE **)malloc(local_rows * sizeof(DATA_TYPE *));
-    B = (DATA_TYPE **)malloc(local_rows * sizeof(DATA_TYPE *));
+    X = (DATA_TYPE **)malloc((local_rows + 2) * sizeof(DATA_TYPE *));
+    A = (DATA_TYPE **)malloc((local_rows + 2) * sizeof(DATA_TYPE *));
+    B = (DATA_TYPE **)malloc((local_rows + 2) * sizeof(DATA_TYPE *));
 
-    for (int i = 0; i < local_rows; i++)
+    for (int i = 0; i < local_rows + 2; i++)
     {
         X[i] = (DATA_TYPE *)malloc(N * sizeof(DATA_TYPE));
         A[i] = (DATA_TYPE *)malloc(N * sizeof(DATA_TYPE));
@@ -39,9 +38,9 @@ static void init_arrays()
     {
         for (j = 0; j < N; j++)
         {
-            X[i][j] = ((DATA_TYPE)(rank * local_rows + i) * (j + 1) + 1) / N;
-            A[i][j] = ((DATA_TYPE)(rank * local_rows + i) * (j + 2) + 2) / N;
-            B[i][j] = ((DATA_TYPE)(rank * local_rows + i) * (j + 3) + 3) / N;
+            X[i + 1][j] = ((DATA_TYPE)(rank * local_rows + i) * (j + 1) + 1) / N;
+            A[i + 1][j] = ((DATA_TYPE)(rank * local_rows + i) * (j + 2) + 2) / N;
+            B[i + 1][j] = ((DATA_TYPE)(rank * local_rows + i) * (j + 3) + 3) / N;
         }
     }
 }
@@ -49,7 +48,7 @@ static void init_arrays()
 // Функция освобождения памяти массивов X, A и B
 static void free_arrays()
 {
-    for (int i = 0; i < local_rows; i++)
+    for (int i = 0; i < local_rows + 2; i++)
     {
         free(X[i]);
         free(A[i]);
@@ -77,7 +76,7 @@ static void print_row(int row)
         printf("Rank %d, row %d:\n", valid_rank, row);
 
         for (int i = 0; i < N; i++)
-            printf("%f \n", X[row][i]);
+            printf("%f \n", X[row + 1][i]);
     }
 }
 
@@ -107,17 +106,13 @@ static void print_array(int n, DATA_TYPE **X, int rank, int size)
 static void kernel_adi(int tsteps, int n, DATA_TYPE **X, DATA_TYPE **A, DATA_TYPE **B, int rank, int size, int local_rows)
 {
     int t, i1, i2, q;
-    DATA_TYPE *quantum_X = (DATA_TYPE *)malloc(quantum_size * sizeof(DATA_TYPE));//буфер для передачи кванта
-    DATA_TYPE *quantum_B = (DATA_TYPE *)malloc(quantum_size * sizeof(DATA_TYPE));
-    DATA_TYPE *temp_X = (DATA_TYPE *)malloc(N * sizeof(DATA_TYPE));// буфер для паредачи полной строки
-    DATA_TYPE *temp_A = (DATA_TYPE *)malloc(N * sizeof(DATA_TYPE));
     MPI_Status statuses[2];
     MPI_Request requests[2];
 
     for (t = 0; t < tsteps; t++)
     {
         // Горизонтальные обновления
-        for (i1 = 0; i1 < local_rows; i1++)
+        for (i1 = 1; i1 < local_rows + 1; i1++)
         {
             for (i2 = 1; i2 < n; i2++)
             {
@@ -129,7 +124,7 @@ static void kernel_adi(int tsteps, int n, DATA_TYPE **X, DATA_TYPE **A, DATA_TYP
         }
 
         // Обратная подстановка для локальных строк
-        for (int i1 = 0; i1 < local_rows; i1++)
+        for (int i1 = 1; i1 < local_rows + 1; i1++)
         {
             for (i2 = 0; i2 < n - 2; i2++)
             {
@@ -141,53 +136,35 @@ static void kernel_adi(int tsteps, int n, DATA_TYPE **X, DATA_TYPE **A, DATA_TYP
         // Вертикальные обновления с конвейерной обработкой
         for (q = 0; q < num_quanta; q++)
         {
-            int start = q * quantum_size;// с какого элемента начинается данный квант
-            int end = start + quantum_size;// на каком элементе заканчивается данный квант
+            int start = q * quantum_size;   // с какого элемента начинается данный квант
+            int end = start + quantum_size; // на каком элементе заканчивается данный квант
 
-            end = (end > N) ? N : end;// ограничение квантов
+            end = (end > N) ? N : end; // ограничение квантов
+            int current_quantum_size = end - start;
 
             // Получение данных от предыдущего процесса
             if (rank > 0)
             {
-                MPI_Irecv(quantum_X, quantum_size, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &requests[0]);
-                MPI_Irecv(quantum_B, quantum_size, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, &requests[1]);
+                MPI_Irecv(&X[0][start], current_quantum_size, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &requests[0]);
+                MPI_Irecv(&B[0][start], current_quantum_size, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, &requests[1]);
                 MPI_Waitall(2, requests, statuses);
             }
 
             // Обработка текущего кванта
-            for (i1 = 0; i1 < local_rows; i1++)
+            for (i1 = (rank == 0 ? 2 : 1); i1 < local_rows + 1; i1++)
             {
-                if (i1 == 0)
+                for (i2 = start; i2 < end; i2++)
                 {
-                    if (rank > 0)
-                    {
-                        for (i2 = start; i2 < end; i2++)
-                        {
-                            X[i1][i2] = X[i1][i2] - quantum_X[i2 - start] * A[i1][i2] / quantum_B[i2 - start];
-                            B[i1][i2] = B[i1][i2] - A[i1][i2] * A[i1][i2] / quantum_B[i2 - start];
-                        }
-                    }
-                }
-                else
-                {
-                    for (i2 = start; i2 < end; i2++)
-                    {
-                        X[i1][i2] = X[i1][i2] - X[i1 - 1][i2] * A[i1][i2] / B[i1 - 1][i2];
-                        B[i1][i2] = B[i1][i2] - A[i1][i2] * A[i1][i2] / B[i1 - 1][i2];
-                    }
+                    X[i1][i2] = X[i1][i2] - X[i1 - 1][i2] * A[i1][i2] / B[i1 - 1][i2];
+                    B[i1][i2] = B[i1][i2] - A[i1][i2] * A[i1][i2] / B[i1 - 1][i2];
                 }
             }
 
             // Отправка данных следующему процессу
             if (rank < size - 1)
             {
-                for (i2 = start; i2 < end; i2++)
-                {
-                    quantum_X[i2 - start] = X[local_rows - 1][i2];
-                    quantum_B[i2 - start] = B[local_rows - 1][i2];
-                }
-                MPI_Isend(quantum_X, quantum_size, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &requests[0]);
-                MPI_Isend(quantum_B, quantum_size, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &requests[1]);
+                MPI_Isend(&X[local_rows][start], current_quantum_size, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &requests[0]);
+                MPI_Isend(&B[local_rows][start], current_quantum_size, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &requests[1]);
                 MPI_Waitall(2, requests, statuses);
             }
         }
@@ -197,53 +174,35 @@ static void kernel_adi(int tsteps, int n, DATA_TYPE **X, DATA_TYPE **A, DATA_TYP
         {
             for (i2 = 0; i2 < n; i2++)
             {
-                X[local_rows - 1][i2] = X[local_rows - 1][i2] / B[local_rows - 1][i2];
+                X[local_rows][i2] = X[local_rows][i2] / B[local_rows][i2];
             }
         }
 
         // Обратная подстановка для вертикальных обновлений с обменом данными
-        if (size > 1 && rank > 0)
+        if (rank > 0)
         {
-            MPI_Irecv(temp_X, n, MPI_DOUBLE, rank - 1, 2, MPI_COMM_WORLD, &requests[0]);
-            MPI_Irecv(temp_A, n, MPI_DOUBLE, rank - 1, 3, MPI_COMM_WORLD, &requests[1]);
+            MPI_Irecv(X[0], n, MPI_DOUBLE, rank - 1, 2, MPI_COMM_WORLD, &requests[0]);
+            MPI_Irecv(A[0], n, MPI_DOUBLE, rank - 1, 3, MPI_COMM_WORLD, &requests[1]);
             MPI_Waitall(2, requests, statuses);
         }
 
-        for (i1 = local_rows - 1; i1 >= 0; i1--)
+        if (rank < size - 1)
         {
-            if (i1 == (local_rows - 1) && rank < size - 1)
-            {
-                MPI_Isend(X[local_rows - 1], n, MPI_DOUBLE, rank + 1, 2, MPI_COMM_WORLD, &requests[0]);
-                MPI_Isend(A[local_rows - 1], n, MPI_DOUBLE, rank + 1, 3, MPI_COMM_WORLD, &requests[1]);
-                MPI_Waitall(2, requests, statuses);
-            }
+            MPI_Isend(X[local_rows], n, MPI_DOUBLE, rank + 1, 2, MPI_COMM_WORLD, &requests[0]);
+            MPI_Isend(A[local_rows], n, MPI_DOUBLE, rank + 1, 3, MPI_COMM_WORLD, &requests[1]);
+            MPI_Waitall(2, requests, statuses);
+        }
 
-            if (i1 == 0)
+        for (i1 = local_rows - (rank == (size - 1) ? 1 : 0); i1 >= (rank == 0 ? 2 : 1); i1--)
+        {
+            for (i2 = 0; i2 < n; i2++)
             {
-                if (rank > 0)
-                {
-                    for (i2 = 0; i2 < n; i2++)
-                    {
-                        X[i1][i2] = (X[i1][i2] - temp_X[i2] * temp_A[i2]) / B[i1][i2];
-                    }
-                }
-            }
-            else if (rank < size - 1 || ((rank == size - 1) && i1 <= local_rows - 2))
-            {
-                for (i2 = 0; i2 < n; i2++)
-                {
-                    X[i1][i2] = (X[i1][i2] - X[i1 - 1][i2] * A[i1 - 1][i2]) / B[i1][i2];
-                }
+                X[i1][i2] = (X[i1][i2] - X[i1 - 1][i2] * A[i1 - 1][i2]) / B[i1][i2];
             }
         }
 
         MPI_Barrier(MPI_COMM_WORLD);
     }
-
-    free(quantum_X);
-    free(quantum_B);
-    free(temp_X);
-    free(temp_A);
 }
 
 int main(int argc, char **argv)
@@ -263,8 +222,12 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    local_rows = (N / size) + (rank < (N % size) ? 1 : 0);// количество элементов на 1 процессе
-    quantum_size = (N / num_quanta) + ((N % num_quanta > 0) ? 1 : 0);//количество элементов на 1 кванте
+    num_quanta = num_quanta < 1 ? 1 : num_quanta;
+    num_quanta = num_quanta > N ? N : num_quanta;
+
+    local_rows = (N / size) + (rank < (N % size) ? 1 : 0);  // количество элементов на 1 процессе
+    quantum_size = N / num_quanta;                          // количество элементов на 1 кванте
+    num_quanta = N / quantum_size;                          // количество квантов пересчитаное с учетом размера кванта
 
     init_arrays();
 
@@ -277,11 +240,11 @@ int main(int argc, char **argv)
     {
         printf("\nN: %d", N);
         printf("\nQuantum size: %d", quantum_size);
-        printf("\nNumber of quanta: %d", N / quantum_size);
+        printf("\nNumber of quanta: %d", num_quanta);
         printf("\nTotal execution time: %f seconds\n", time1 - time0);
     }
 
-    // print_row(31);
+    print_row(31);
 
     free_arrays();
 
